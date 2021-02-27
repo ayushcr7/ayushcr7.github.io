69dd39e0eaddb553407cb8084bf030be56302b64
ayushcr7 <ayushchoudhary91@gmail.com>
2021-02-27 22:12:09 +0530

    d
    Descr

---
+++ SentimentAnalysisWebAPI/Controllers/PredictController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.ML;
+using SentimentAnalysisWebAPI.DataModels;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SentimentAnalysisWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class PredictController : ControllerBase
+    {
+        private readonly PredictionEnginePool<SentimentData, SentimentPrediction> _predictionEnginePool;
+
+        public PredictController(PredictionEnginePool<SentimentData, SentimentPrediction> predictionEnginePool)
+        {
+            _predictionEnginePool = predictionEnginePool;
+        }
+
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] SentimentData input)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            SentimentPrediction prediction = _predictionEnginePool.Predict(modelName: "SentimentAnalysisModel", example: input);
+
+            string sentiment = Convert.ToBoolean(prediction.Prediction) ? "Positive" : "Negative";
+
+            return Ok(sentiment);
+        }
+    }
+}
